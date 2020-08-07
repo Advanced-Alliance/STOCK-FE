d819701014217ef07051f83ac4db3a6a81803f3c
Haehiro <47781727+Haehiro@users.noreply.github.com>
2020-08-07 18:28:22 +0300

    Upadate Angular and Packages config and urls (#1)
    * packages updates and urls changes
    
    * update angular deps

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,31 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import {AnswersService} from '../answers.service';
+import { Component, OnInit } from "@angular/core";
+import { AnswersService } from "../answers.service";
 
 @Component({
-  selector: 'app-admin',
-  templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  selector: "app-admin",
+  templateUrl: "./admin.component.html",
+  styleUrls: ["./admin.component.css"],
 })
 export class AdminComponent implements OnInit {
-
   questions: any;
 
-  constructor(private answersService: AnswersService) {
-  }
+  constructor(private answersService: AnswersService) {}
 
   ngOnInit() {
-    this.answersService.getAnswers().subscribe(
-      res => {
-        this.questions = res;
-      }
-    );
+    this.answersService.getAnswers().subscribe((res) => {
+      this.questions = res;
+    });
   }
 
-  private saveChanges() {
-    this.answersService.saveQuestions(this.questions).subscribe(
-      res => {
-      }
-    );
+  public saveChanges() {
+    this.answersService.saveQuestions(this.questions).subscribe((res) => {});
   }
-
 }
