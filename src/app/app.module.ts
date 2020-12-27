3bfd5cb2a8551c622e5d707f8fbd0a94bdf8dd62
ShadowHD33RUS <ShadowHD33RUS@gmail.com>
2020-12-28 02:44:50 +0300

    minor refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,3 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 
@@ -7,7 +5,6 @@ import { AppComponent } from './app.component';
 import { AnswerCardComponent } from './answer-card/answer-card.component';
 import { IndicatorComponent } from './indicator/indicator.component';
 import { AnswersService } from './answers.service';
-import { AdminComponent } from './pages/admin/admin.component';
 
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
