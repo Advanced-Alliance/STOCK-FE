60d98a000bc55da5c7638e69a2afec22ef42cac8
ShadowHD <ShadowHD33RUS@gmail.com>
2020-12-27 17:52:01 +0300

    #FEUD-31: [FE] Enable strictNullChecks parameter in tsconfig (#4)
    * Add strictNullChecks parameter
    
    * Fix compiling errors

--- src/app/indicator/indicator.component.ts
+++ src/app/indicator/indicator.component.ts
@@ -1,32 +1,32 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
-    selector: 'app-indicator',
-    styleUrls: ['indicator.component.css'],
-    templateUrl: './indicator.component.html'
+  selector: 'app-indicator',
+  styleUrls: ['indicator.component.css'],
+  templateUrl: './indicator.component.html'
 })
 
 export class IndicatorComponent {
-    @Input()
-    private indicatorColor: number[];
+  @Input()
+  private indicatorColor: number[];
 
-    @Output()
-    private failedAnswerCb: EventEmitter<any> = new EventEmitter();
+  @Output()
+  private failedAnswerCb: EventEmitter<any> = new EventEmitter();
 
-    public getIndicatorClassName(id: number): string {
-        return indicatorClassNameMap.get(this.indicatorColor[id]);
-    }
-
-    public onFailed(id: number) {
-      if (this.indicatorColor[id] === 3) {
-        return;
-      }
-      this.failedAnswerCb.emit(id);
-    }
-}
-
-const indicatorClassNameMap = new Map<number, string>([
+  indicatorClassNameMap = new Map<number, string>([
     [1, 'indicator-green circle'],
     [2, 'indicator-yellow circle'],
     [3, 'indicator-red circle']
-]);
+  ]);
+
+  public getIndicatorClassName(id: number): string {
+    return this.indicatorClassNameMap.get(this.indicatorColor[id]) || 'indicator-green circle';
+  }
+
+  public onFailed(id: number) {
+    if (this.indicatorColor[id] === 3) {
+      return;
+    }
+    this.failedAnswerCb.emit(id);
+  }
+}
