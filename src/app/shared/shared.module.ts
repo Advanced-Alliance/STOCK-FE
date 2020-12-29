b85ce93263a25624d7f2390be79b810dc8a2bd56
ShadowHD33RUS <ShadowHD33RUS@gmail.com>
2020-12-29 16:27:23 +0300

    add styles

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,9 +5,13 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 const MATERIAL = [
   MatButtonModule,
+  MatIconModule,
+  MatFormFieldModule,
 ];
 
 @NgModule({
